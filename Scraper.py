c1b81a90b3dbfc62d9a7870fbd6abfb09366994f
QuickSorting <victor.kojuharov@hotmail.com>
2021-08-17 22:51:07 +0300

    Make the program check whether the article is from the Washington Post

--- Scraper.py
+++ Scraper.py
@@ -5,6 +5,9 @@ from bs4 import BeautifulSoup
 if len(sys.argv) != 2:
     print("Please provide the URL of the website as an argument")
     sys.exit()
+if not sys.argv[1].startswith("https://www.washingtonpost.com"):
+    print("This scraper works only with Washington Post articles. Please make sure that the provided URL is from the Washington Post")
+    sys.exit()
 
 URL = sys.argv[1]
 page = requests.get(URL)
